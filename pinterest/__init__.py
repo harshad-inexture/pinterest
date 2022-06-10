deb96f2e17c2a80cb73f02216a8bbe4a550ffc5e
Harshad Vaghasiya <hpvaghasiya8@gmail.com>
2022-06-10 10:18:15 +0530

    make migration

--- pinterest/__init__.py
+++ pinterest/__init__.py
@@ -3,12 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from pinterest.config import Config
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # configs---------------------------------------------------------------------------------
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view='login_page'
