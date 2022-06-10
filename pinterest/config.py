f157d7d21a2eaaf196e582f99e6c99e1179ec66b
Harshad Vaghasiya <hpvaghasiya8@gmail.com>
2022-06-10 11:24:01 +0530

    update procfile

--- pinterest/config.py
+++ pinterest/config.py
@@ -4,5 +4,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
