83bddbb93d48e4b738b4c6efd30bf7b5a448cccd
Harshad Vaghasiya <hpvaghasiya8@gmail.com>
2022-06-11 10:59:14 +0530

    add profile

--- pinterest/form.py
+++ pinterest/form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired , Length, Email, EqualTo,ValidationError
-from pinterest.models import User
+from pinterest.models import User, Tags
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=2,max=25)])
@@ -20,6 +22,29 @@ class RegistrationForm(FlaskForm):
         if sameemail:
             raise ValidationError('That email is taken please choose another one.')
 
+# class MultiCheckboxField(SelectMultipleField):
+#     widget = widgets.ListWidget(prefix_label=False)
+#     option_widget = widgets.CheckboxInput()
+
+class UpdateAccForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=25)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    profile_pic = FileField('Update profile picture', validators=[FileAllowed(['jpg','png'])])
+    # tags = Tags.query.all()
+    # interests = MultiCheckboxField(choices=tags)
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            sameuser = User.query.filter_by(username=username.data).first()
+            if sameuser:
+                raise ValidationError('That username is taken please choose another one.')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            sameemail = User.query.filter_by(email=email.data).first()
+            if sameemail:
+                raise ValidationError('That email is taken please choose another one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
--- pinterest/routes.py
+++ pinterest/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template,flash,redirect,url_for,request
-from pinterest.form import RegistrationForm,LoginForm
+from pinterest.form import RegistrationForm,LoginForm,UpdateAccForm
 from pinterest.models import User,Pin,UserInterest,Tags
 from pinterest import app,db,bcrypt
+import secrets,os
+from PIL import Image
 from flask_login import login_user,current_user,logout_user,login_required
 
 # routes-----------------------------------------------------------------------------------
@@ -10,10 +12,63 @@ from flask_login import login_user,current_user,logout_user,login_required
 def home_page():
     return render_template('home.html')
 
-@app.route("/profile")
+def save_pic(form_pic):
+    random_hex = secrets.token_hex(8)
+    _,f_ext = os.path.splitext(form_pic.filename)
+    profile_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_img',profile_fn)
+
+    pic_size = (125,125)
+    i = Image.open(form_pic)
+    i.thumbnail(pic_size)
+
+    i.save(picture_path)
+    return profile_fn
+
+def fatch_user_tags(user_tags):
+    selected_tags=[]
+    for i in user_tags:
+        selected_tags.append(i.tag_id)
+    return selected_tags
+
+@app.route("/profile", methods=['POST','GET'])
 @login_required
 def profile_page():
-    return render_template('profile.html',title='profile')
+    form = UpdateAccForm()
+    tags = Tags.query.all()
+    user_tags = UserInterest.query.filter_by(user_id = current_user.id).all()
+    selected_tags = fatch_user_tags(user_tags)
+    interests = request.form.getlist('interests')
+    if form.validate_on_submit():
+        if form.profile_pic.data:
+            profile_name = save_pic(form.profile_pic.data)
+            current_user.profile_pic = profile_name
+
+        if selected_tags != interests:
+            # for delete interest--------------------
+            for selected_tag in selected_tags:
+                if selected_tag not in interests:
+                    UserInterest.query.filter_by(tag_id = selected_tag,user_id=current_user.id).delete()
+                    db.session.commit()
+
+            # for update add new interests------------------------
+            for interest_id in interests:
+                if interest_id not in selected_tags:
+                    user_interest = UserInterest(user_id=current_user.id, tag_id=interest_id)
+                    db.session.add(user_interest)
+
+        current_user.username=form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.!','success')
+        return redirect(url_for('profile_page'))
+
+    # current user data------------------------------------------
+    elif request.method=='GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    profile_pic = url_for('static',filename='profile_img/' + current_user.profile_pic)
+    return render_template('profile.html',title='profile',form=form,profile_pic=profile_pic,tags=tags,selected_tags=selected_tags)
 
 @app.route("/register", methods=['POST','GET'])
 def register_page():
