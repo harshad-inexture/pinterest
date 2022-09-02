19b9de7e10b4b5bd8d7e4a80017a6b713e3ac55f
Harshad Vaghasiya <hpvaghasiya8@gmail.com>
2022-09-02 17:10:53 +0530

    update msg.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from pinterest import create_app, celery
 from pinterest.celery_utils import init_celery
 
-# if __name__ == '__main__':
-#     app = create_app()
-#     init_celery(celery, app)
-#     app.run()
-
 app = create_app()
-init_celery(celery, app)
+if __name__ == '__main__':
+    init_celery(celery, app)
+    app.run()
 
